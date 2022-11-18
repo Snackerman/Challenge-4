524b8be65c1301fe8e84ca61c01a53105a9dc0d6
Ben <benackdrums@gmail.com>
2022-11-17 23:41:30 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// Clicking the start button starts the Game
+// Clicking the start button starts counting down a Timer
+// Clicking the start button shows a qustion
+// Clicking an answer button calculates score and presents the next question
+// 
+
+
+
+
+
+
+
+
+var questions = [
+    (title:"")    
+
+
+
+]
+
+
+function startGame() {
+   
+}
+
+function startTimer() {
+
+}
+
+function showQuestions() {
+
+}
+
+function endGame() {
+
+}
+
